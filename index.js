9d5c0999a9e02ab82d6951143f3816b814e66cdc
Valurven <78742039+Valurven@users.noreply.github.com>
2022-06-15 10:36:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -84,8 +84,6 @@ function brewingMenuHandler(){
 
 fetch("/ProgramowanieWInternecie/language/en.json")
 .then(response =>{
-  console.log(response.json());
-  console.log(response.json().test);
    return response.json();
 })
 .then(response =>{
